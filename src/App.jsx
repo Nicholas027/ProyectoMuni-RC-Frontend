475d8845e579f565c9cc3c92f4ab4abb893a9cc6
Andres Santamarina <andresesantamarina@gmail.com>
2024-04-12 11:58:22 -0300

    agregue el video como background y un wrapper para mostrar el mensaje de error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Error404 from "../src/components/pages/Error404.jsx"
 
 function App() {
 
   return (
     <>
-    
+    <Error404/>
     </>
   )
 }
---
+++ src/components/pages/Error404.jsx
@@ -0,0 +1,65 @@
+import videoError from "../../assets/bgError404.mp4";
+import Button from "react-bootstrap/Button";
+import "../../styles/error404.css";
+
+const Error404 = () => {
+  return (
+    <section>
+      <div className="wrapper text-center text-white">
+        <h1 className="display-1 tituloError">Error 404</h1>
+        <p className="display-5 mensajeError">Página no encontrada!</p>
+        <Button className="btn btn-light">Volver al inicio</Button>
+        <div className="text-center my-3">
+          <a
+            href="https://twitter.com/Concepcion_Tuc"
+            target="_blank"
+            className="m-2"
+          >
+            <i className="bi bi-twitter-x"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/municipalidadeconcepcion"
+            target="_blank"
+            className="m-2"
+          >
+            <i className="bi bi-facebook"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/municipalidadconcepcion"
+            target="_blank"
+            className="m-2"
+          >
+            <i className="bi bi-instagram"></i>
+          </a>
+          <a
+            href="https://www.youtube.com/@municipalidadconcepcion9109"
+            target="_blank"
+            className="m-2"
+          >
+            <i className="bi bi-youtube"></i>
+          </a>
+          <a
+            href="https://api.whatsapp.com/send/?phone=%2B5493865228080&text&type=phone_number&app_absent=0"
+            target="_blank"
+            className="m-2"
+          >
+            <i className="bi bi-whatsapp"></i>
+          </a>
+          <a
+            href="mailto:prensa@concepcion.gob.ar"
+            target="_blank"
+            className="m-2"
+          >
+            <i className="bi bi-envelope-fill"></i>
+          </a>
+        </div>
+      </div>
+      <div className="overlay"></div>
+      <video autoPlay loop muted id="videoError">
+        <source src={videoError} type="video/mp4" />
+      </video>
+    </section>
+  );
+};
+
+export default Error404;
