cfb2a09a258ee8a53ee30b72cd97ad0687b2ed8b
Andres Santamarina <andresesantamarina@gmail.com>
2024-04-12 11:45:49 -0300

    agregue el componente footer y le puse los estilos necesarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Footer from "../src/components/common/Footer.jsx";
 
 function App() {
-  
-
   return (
     <>
-     
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
