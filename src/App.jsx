0044f26218643ad0319022fc7b5a11dc6cddcc29
Andres Santamarina <andresesantamarina@gmail.com>
2024-04-12 11:30:22 -0300

    agregue el navbar, con el logo de la muni y sin el link a administrar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import MenuNav from './components/common/MenuNav';
 
 function App() {
   
 
   return (
     <>
-    
+    <MenuNav/>
     </>
   )
 }
