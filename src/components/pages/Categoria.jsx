00c381025968029006b79df5ec3fae33ef8be918
Andres Santamarina <andresesantamarina@gmail.com>
2024-04-13 06:23:42 -0300

    agregue el dropdown para ver las categorias y le cambie la fuente

--- src/components/pages/Categoria.jsx
+++ src/components/pages/Categoria.jsx
@@ -1,11 +1,29 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Dropdown } from "react-bootstrap";
 import CardProfesional from "./profesional/CardProfesional";
 import "../../styles/categoria.css";
 
 const Categoria = () => {
   return (
     <Container className="mainSection my-5">
-      <h1 className="text-center tituloCategoria">Electricistas</h1>
+      <h1 className="text-center tituloCategoria">ELECTRICISTAS</h1>
+      <Dropdown className="d-flex justify-content-end">
+        <Dropdown.Toggle variant="secondary" id="dropdownCategorias" size="sm">
+          CATEGORÍAS
+        </Dropdown.Toggle>
+        <Dropdown.Menu className="dropdownMenu">
+          <Dropdown.Item href="#/carpinteros">Carpinteros</Dropdown.Item>
+          <Dropdown.Item href="#/gasistas">Gasistas</Dropdown.Item>
+          <Dropdown.Item href="#/cerrajeros">Cerrajeros</Dropdown.Item>
+          <Dropdown.Item href="#/mecanicos">Mecánicos</Dropdown.Item>
+          <Dropdown.Item href="#/electricistas">Electricistas</Dropdown.Item>
+          <Dropdown.Item href="#/albaniles">Albañiles</Dropdown.Item>
+          <Dropdown.Item href="#/plomeros">Plomeros</Dropdown.Item>
+          <Dropdown.Item href="#/pintores">Pintores</Dropdown.Item>
+          <Dropdown.Item href="#/herreros">Herreros</Dropdown.Item>
+          <Dropdown.Item href="#/jardineros">Jardineros</Dropdown.Item>
+          <Dropdown.Item href="#/otros">Otro</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
       <Row className="my-4">
         <CardProfesional />
         <CardProfesional />
