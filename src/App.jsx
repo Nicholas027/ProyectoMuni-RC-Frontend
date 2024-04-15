86f2fb84234f96b44ed03512de2f54715d8fa0dc
Alejo Herrera <alejoherrera1166@gmail.com>
2024-04-15 06:37:04 -0300

    Componente ProfesionalDetail creado.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import Error404 from "../src/components/pages/Error404.jsx";
 import Index from "../src/components/pages/Index.jsx";
 import Footer from "../src/components/common/Footer.jsx";
 import MenuNav from './components/common/MenuNav';
+import ProfesionalDetail from "./components/pages/ProfesionalDetail.jsx";
 
 
 function App() {
   return (
     <>
       <MenuNav/>
-      <Index />
+      <ProfesionalDetail />
       <Footer />
     </>
   );
---
+++ src/components/pages/ProfesionalDetail.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const ProfesionalDetail = () => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+};
+
+export default ProfesionalDetail;
