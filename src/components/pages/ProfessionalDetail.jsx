7fec82b2e6c6f203ea0bc371652ca14d0b0c51eb
Alejo Herrera <alejoherrera1166@gmail.com>
2024-04-15 14:26:28 -0300

    Foto de perfil y portada responsivos.

--- src/components/pages/ProfessionalDetail.jsx
+++ src/components/pages/ProfessionalDetail.jsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import imagenPortada from "../../assets/categoryLogos/albanilLogo.webp";
 import "../../styles/ProfessionalDetail.css";
 
@@ -6,14 +6,12 @@ const ProfessionalDetail = () => {
   return (
     <main className="">
       <section className="m-4 p-2 fondoFotos">
-        <div className="">
-          <img
-            src={imagenPortada}
-            alt="Foto de portada con la categoria del profesional."
-            height={200}
-            className="cajaPortada"
-          />
-        </div>
+        <img
+          src={imagenPortada}
+          alt="Foto de portada con la categoria del profesional."
+          height={200}
+          className="cajaPortada"
+        />
         <div className="d-flex justify-content-center cajaPerfil">
           <div className="estiloFotoPerfil">
             <img
@@ -23,15 +21,25 @@ const ProfessionalDetail = () => {
             />
           </div>
         </div>
-        <Container className="mt-5 text-center">
-          <p className="mt-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Repellendus maxime unde animi cupiditate culpa a? Illum earum
-            ratione eligendi vel eum quisquam quae, dolores neque consectetur,
-            optio, excepturi maxime facere. Placeat pariatur doloribus maxime
-            perspiciatis. Maxime quod consequatur vel odio nemo corporis ipsa
-            cumque fuga, labore quas. Vitae iusto dolorum suscipit laboriosam
-            consectetur eos rerum delectus nemo, qui explicabo accusamus?
+        <Container className="text-center">
+          <h1>
+            Facundo Alejo Herrera <br />
+          </h1>
+          <span className="categoria my-2 px-1 text-light">Albañil</span>
+          <div>⭐⭐⭐⭐⭐</div>
+          <Button className="my-3 py-1 px-5 btnContacto">Contactar</Button>
+          <p className="h5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sint
+            modi neque libero, sapiente commodi velit perspiciatis facilis
+            laborum fugiat, autem aperiam. Vel ipsum doloremque repudiandae
+            magnam temporibus reiciendis quam? Sint, consectetur voluptas
+            maiores nihil commodi deserunt, dolorum numquam tempora assumenda
+            nulla quibusdam soluta illum dolorem. Suscipit impedit aliquid unde
+            incidunt similique laboriosam quidem voluptates nemo fugiat. Eos,
+            animi doloremque! Quod reiciendis repellendus qui, sequi harum
+            aliquam incidunt cumque culpa doloremque perspiciatis iusto nam
+            nesciunt quis esse ipsam ea ipsum itaque adipisci doloribus
+            consectetur animi asperiores velit ab. Facere, ea.
           </p>
         </Container>
       </section>
