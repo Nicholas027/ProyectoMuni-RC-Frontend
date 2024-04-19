7c38122bef46b4dca50cc3a8dbde1ba7f145273f
Nicholas027 <nicholas0810152015@gmail.com>
2024-04-18 22:33:57 -0300

    dar alta profesional

---
+++ src/components/pages/administrador/DarAltaProfesional.jsx
@@ -0,0 +1,209 @@
+import "../../../styles/signUp.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import logoMuni from "../../../assets/logo_muni_vertical_AZUL.png";
+import { useForm } from "react-hook-form";
+import { Container } from "react-bootstrap";
+import useTitle from "../../../hooks/useTitle";
+
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  useTitle("Añadir nuevo profesional")
+
+  const onSubmit = async (nuevoProfesional) => {
+    console.log(nuevoProfesional);
+  };
+
+  return (
+    <>
+      <div className="position-relative">
+        <div className="backgroundSignUp">
+          <img
+            src="https://scontent.faep6-1.fna.fbcdn.net/v/t39.30808-6/251378801_4826907517342542_8931559505337231654_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeF9SiF35dffz4DalMzMoWP6vB_XlfWWEg68H9eV9ZYSDopXGgUimN4ZFjsGdm4-PIotal7hjaSqn4EPn_4SBKe5&_nc_ohc=UD3s6L6s1iEAb440Che&_nc_ht=scontent.faep6-1.fna&oh=00_AfCg2zMSinVwYbu6ijCcbD1DaelARCh9XEkBEcyhvtsYow&oe=6625F8BC"
+            alt="imgProducto"
+          />
+        </div>
+
+        <Container className="boxContent rounded-4">
+          <div className="title">
+            <img
+              src={logoMuni}
+              alt="imgProducto"
+              className="rounded-start-4 "
+              width={140}
+              height={155}
+            />
+            <h1>REGISTRAR NUEVO PROFESIONAL</h1>
+          </div>
+
+          <div className="formContent">
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Label>Nombre Completo</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese el nombre y apellido"
+                  {...register("nombreCompleto", {
+                    required: "Ingrese el nombre completo",
+                    minLength: {
+                      value: 8,
+                      message: "Ingrese el nombre con mínimo 2 caracteres",
+                    },
+                    maxLength: {
+                      value: 100,
+                      message: "Ingrese el nombre con máximo 50 caracteres",
+                    },
+                  })}
+                />
+                <Form.Text className="text-danger">
+                  {errors.nombreCompleto?.message}
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicDni">
+                <Form.Label>DNI (sin puntos)</Form.Label>
+                <Form.Control
+                  type="number" 
+                  placeholder="10000000"
+                  {...register("dni", {
+                    required: "Ingrese el DNI",
+                    min: {
+                        value: 10000000,
+                        message: "Ingrese un dni valido",
+                      },
+                      max: {
+                        value: 99999999,
+                        message: "Ingrese un dni valido",
+                      },
+                  })}
+                />
+                <Form.Text className="text-danger">
+                  {errors.dni?.message}
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Foto</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese una dirección de foto"
+                  {...register("foto", {
+                    required: "Ingrese su foto",
+                    pattern: {
+                      value:
+                        /(http)=?s?:?(\/\/[^"'"]*\.(?:png|jpg|jpeg|gif|svg))/i,
+                      message:
+                        "Ingrese una url de una imagen png, jpg, gif, svg",
+                    },
+                  })}
+                />
+                <Form.Text className="text-danger">
+                  {errors.foto?.message}
+                </Form.Text>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Ingrese el email"
+                  {...register("email", {
+                    required: "Ingrese el email",
+                    pattern: {
+                      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                      message: "Email o Contraseña incorrecta",
+                    },
+                  })}
+                />
+                <Form.Text className="text-danger">
+                  {errors.email?.message}
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Ingrese la contraseña"
+                  {...register("password", {
+                    required: "Ingrese la contraseña",
+                    pattern: {
+                      value: /^(?=.*[A-Z])(?=.*\d).{6,20}$/,
+                      message: "La contraseña debe minimo 6 caracteres, Una mayuscula y un numero",
+                    },
+                  })}
+                />
+                <Form.Text className="text-danger">
+                  {errors.password?.message}
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Label>CV</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese su CV"
+                  {...register("cv", {
+                    required: "Ingrese su cv",
+                  })}
+                />
+                <Form.Text className="text-danger">
+                  {errors.cv?.message}
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formCategoria">
+                <Form.Label>Categoría (Profesión que ejerce):</Form.Label>
+                <Form.Select
+                  {...register("categoria", {
+                    required: "Seleccione una Categoría",
+                  })}
+                >
+                  <option value="">Seleccione una Opción</option>
+                  <option value="Carpintero">Carpintero</option>
+                  <option value="Gasista">Gasista</option>
+                  <option value="Cerrajero">Cerrajero</option>
+                  <option value="Mecanico">Mecánico</option>
+                  <option value="Electricista">Electricista</option>
+                  <option value="Albañil">Albañil</option>
+                  <option value="Plomero">Plomero</option>
+                  <option value="Pintor">Pintor</option>
+                  <option value="Herrero">Herrero</option>
+                  <option value="Jardinero">Jardinero</option>
+                  <option value="Otros">Otros</option>
+                </Form.Select>
+                <Form.Text className="text-danger">
+                  {errors.categoria?.message}
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Número de Teléfono (sin 15 ni 0 ni -)</Form.Label>
+                <Form.Control
+                  type="tel"
+                  placeholder="3865505050"
+                  {...register("telefono", {
+                    required: "Ingrese su telefono",
+                  })}
+                />
+                <Form.Text className="text-danger">
+                  {errors.tel?.message}
+                </Form.Text>
+              </Form.Group>
+
+              <div className="btnConteiner">
+                <Button className="btnPrincipal" type="submit">Registrar nuevo profesional</Button>
+              </div>
+            </Form>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -17,4 +17,24 @@ export const obtenerProfesionalesAPI = async () => {
       throw new Error(`Error al obtener los profesionales: ${error.message}`);
     }
   };
+
+
+//Petición para dar de alta a un profesional desde el componente administrador.
+export const modificarEstadoProfesionalAPI = async (profesionalId, nuevoEstado) => {
+  try {
+    const response = await fetch(`${URI_Profesionales}/${profesionalId}/state`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ pendiente: nuevoEstado })
+    });
+    if (!response.ok) {
+      throw new Error(`Error al ${nuevoEstado ? 'dar de alta' : 'dar de baja'} al profesional`);
+    }
+    return await response.json();
+  } catch (error) {
+    throw new Error(`Error en la petición para ${nuevoEstado ? 'dar de alta' : 'dar de baja'} al profesional`);
+  }
+};
   
