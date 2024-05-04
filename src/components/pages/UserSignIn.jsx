0932f119d15e4b6abf285ee6e2ca291c989cbfd9
Alejo Herrera <alejoherrera1166@gmail.com>
2024-05-04 20:17:25 -0300

    Componente signIn creado con maquetado terminado.

--- src/components/pages/UserSignIn.jsx
+++ src/components/pages/UserSignIn.jsx
@@ -1,9 +1,74 @@
+import "../../styles/login.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import logoMuni from "../../assets/logo_muni_vectorized.png";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+
 const UserSignIn = () => {
-    return (
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (usuario) => {
+    
+  };
+
+  return (
+    <main className="contenedorLogin">
+      <img
+        src="https://www.eltucumano.com/fotos/cache/notas/2019/02/15/818x460_190215112544_91694.jpg"
+        alt="imgFondoLogin"
+        className="imgFondo"
+      />
+      <Form className="w-25 cardLogin" onSubmit={handleSubmit(onSubmit)}>
         <div>
-            <p>aqui inicia secion el user</p>
+          <img src={logoMuni} alt="" width={200} />
+          <h2 className="mt-3">Iniciar Sesión Usuario</h2>
         </div>
-    );
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Ingrese su email"
+            {...register("email", {
+              required: "Ingrese su email",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Email o Contraseña incorrecta",
+              },
+            })}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Ingrese su contraseña"
+            {...register("pass", {
+              required: "Ingrese su contraseña",
+              pattern: {
+                value: /^(?=.*[A-Z])(?=.*\d).{6,}$/,
+                message: "Email o Contraseña incorrecta",
+              },
+            })}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Recordarme" />
+        </Form.Group>
+        <Form.Text className="text-danger">
+          {errors.pass?.message || errors.email?.message}
+        </Form.Text>
+        <Button type="submit" className="btnPrincipal">
+          Ingresar
+        </Button>
+      </Form>
+    </main>
+  );
 };
 
 export default UserSignIn;
